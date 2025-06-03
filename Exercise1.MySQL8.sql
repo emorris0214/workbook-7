59c72260bd2a233aa6af7b0c9771e9f4c50d41a4
unknown <Enoch Morris>
2025-06-03 10:37:27 -0400

    Completed Exercise 2 of MySQL Learning

--- Exercise1.MySQL8.sql
+++ Exercise1.MySQL8.sql
@@ -40,29 +40,73 @@ ORDER BY ProductName ASC;
 -- Categories
 
 -- 9. Write a query that lists all of the columns and all of the rows of the categories table? What is the category id of seafood?
-SELECT * FROM Categories;
-SELECT CategoryID
-FROM Categories
-WHERE CategoryName = 'seafood';
+SELECT *
+FROM
+	Categories;
+SELECT
+	CategoryID
+FROM
+	Categories
+WHERE
+	CategoryName = 'seafood';
 
 
 -- 10. Examine the Products table. How does it identify the type (category) of each item sold? Write a query to list all of the seafood items we carry.
-
+SELECT
+	p.ProductID, p.ProductName, p.UnitPrice
+FROM
+	Products p
+JOIN
+	Categories c
+ON
+	p.CategoryID = c.CategoryID
+WHERE
+	c.CategoryName = 'Seafood';
 
 
 -- 11. What are the first and last names of all of the Northwind employees?
-
+SELECT
+	FirstName, LastName
+FROM
+	Employees;
+	
 
 -- 12. What employees have "manager" in their titles?
+SELECT
+	FirstName, LastName, Title
+FROM
+	Employees
+WHERE
+	Title LIKE "%manager%";
 
 
 -- 13. List the distinct job titles in employees.
+SELECT
+DISTINCT
+	Title
+FROM
+	Employees;
 
 
--- 14. What employees have a salary that is between $200 0 and $2500?
+-- 14. What employees have a salary that is between $2000 and $2500?
+SELECT
+	EmployeeId, FirstName, LastName, Salary
+FROM
+	Employees
+WHERE
+	Salary BETWEEN 2000 AND 2500;
 
 
 -- 15. List all of the information about all of Northwind's suppliers.
+SELECT *
+FROM
+	Suppliers;
 
-
--- 16. Examine the Products table. How do you know what supplier supplies each product? Write a query to list all of the items that "Tokyo Traders" supplies to Northwind
+-- 16. Examine the Products table. How do you know what supplier supplies each product? Write a query to list all of the items that "Tokyo Traders" supplies to Northwind?
+SELECT
+	p.ProductID, p.ProductName, s.CompanyName
+FROM
+	Products p
+JOIN Suppliers s ON p.SupplierID = s.SupplierID
+WHERE
+	s.CompanyName = "Tokyo Traders";
