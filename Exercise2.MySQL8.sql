33248bf8bd04eb0589c44ef2706e008ac685c98a
unknown <Enoch Morris>
2025-06-03 13:41:02 -0400

    Completed Exercise 2 of MySQL Learning

--- Exercise2.MySQL8.sql
+++ Exercise2.MySQL8.sql
@@ -103,10 +103,5 @@ FROM
 	Suppliers;
 
 -- 16. Examine the Products table. How do you know what supplier supplies each product? Write a query to list all of the items that "Tokyo Traders" supplies to Northwind?
-SELECT
-	p.ProductID, p.ProductName, s.CompanyName
-FROM
-	Products p
-JOIN Suppliers s ON p.SupplierID = s.SupplierID
-WHERE
-	s.CompanyName = "Tokyo Traders";
+SELECT * FROM Products WHERE SupplierID = 4;
+
